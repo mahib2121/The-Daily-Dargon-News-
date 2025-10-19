0c1fce4eebe82792274f9ed7f57428bacab972bf
mahib2121 <khanmahibalam@gmail.com>
2025-10-19 22:45:58 +0600

    Sticky Aded

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -12,7 +12,7 @@ const Layout = () => {
         <div>
             <header>
                 <Hader></Hader>
-                <section className='w-11/12 mx-auto my-4 '>
+                <section className='w-11/12 mx-auto my-4  '>
                     <LatestNews></LatestNews>
                 </section>
                 <nav className='w-11/12 mx-auto my-4'>
@@ -20,13 +20,13 @@ const Layout = () => {
                 </nav>
             </header>
             <main className='w-11/12 mx-auto my-4 grid grid-cols-12' >
-                <aside className='col-span-3'>
+                <aside className='col-span-3  top-0 sticky h-fit'>
                     <LeftSide></LeftSide>
                 </aside>
                 <section className='main col-span-6'>
                     <Outlet></Outlet>
                 </section>
-                <aside className='col-span-3' >
+                <aside className='col-span-3 top-0 sticky h-fit' >
                     <RightSide></RightSide>
                 </aside>
             </main>
